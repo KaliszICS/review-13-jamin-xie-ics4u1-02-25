dd703ee6e1adb330cf915f1b78d770b2994a69b9
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-02-28 04:13:26 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;;
 
 public class PracticeProblem {
 
@@ -6,4 +7,39 @@ public class PracticeProblem {
 
 	}
 
+	public static ArrayList<String> swap(ArrayList<String> list, int num, int num2) {
+		if (num >= 0 && num2 >= 0 && num < list.size() && num2 < list.size()) {
+			String tempString = list.get(num);
+			list.set(num, list.get(num2));
+			list.set(num2, tempString);
+		}
+		return list;
+	}
+
+	public static ArrayList<Double> createArrayList(double[] doubleArray) {
+		ArrayList<Double> doublesList = new ArrayList<>();
+
+		for (int i = 0; i < doubleArray.length; i++) {
+			doublesList.add(doubleArray[i]);
+		}
+		return doublesList;
+	}
+
+	public static HashMap<String, Integer> combineParallelArrays(String[] names, int[] ages) {
+		HashMap<String, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < names.length; i++) {
+			map.put(names[i], ages[i]);
+		}
+
+		return map;
+	}
+
+	public static HashMap<String, Integer> increaseAge(HashMap<String, Integer> map, String name) {
+		if (map.containsKey(name)) {
+			map.put(name, map.get(name) + 1);
+		}
+
+		return map;
+	}
 }
